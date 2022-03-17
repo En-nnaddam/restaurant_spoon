7feeb70836d3025a0e18a9fe2f97df1a94b82e08
En-naddam <abdoennaddam@gmail.com>
2022-03-17 12:40:38 +0100

    renew key

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -24,8 +24,6 @@ const Recipe = () => {
         getRecipe(params.id)
     }, [params.id]);
 
-    console.log(recipe.extendedIngredients);
-
     return (
         <StyledRecipe>
             <div>
